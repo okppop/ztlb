b1e15a58ca3a62024d547ada9d83f251e661d5e4
okppop <okppop@protonmail.com>
2026-03-07 21:11:38 +0800

    feat: config vaildate

--- src/config/config.zig
+++ src/config/config.zig
@@ -4,34 +4,85 @@ const zon = std.zon;
 const CONF_PATH_ENV_KEY: []const u8 = "CONF_PATH";
 const CONF_PATH_DEFAULT: []const u8 = "ztlb.conf";
 
-const Config = struct {
+pub const Config = struct {
     bind: []const Bind,
 
-    fn deinit(self: *Config, allocator: std.mem.Allocator) void {
+    pub fn deinit(self: *Config, allocator: std.mem.Allocator) void {
         for (self.bind) |bind_item| {
             allocator.free(bind_item.address);
 
             switch (bind_item.forward) {
-                .round_robin => |item| free_nested_slice(allocator, u8, item),
-                .weighted_round_robin => |item| {
-                    for (item) |fw| {
-                        allocator.free(fw.target);
-                    }
-                    allocator.free(item);
-                },
-                .ip_hash => |item| free_nested_slice(allocator, u8, item),
-                .least_connections => |item| free_nested_slice(allocator, u8, item),
-                .weighted_least_connections => |item| {
-                    for (item) |fw| {
-                        allocator.free(fw.target);
-                    }
-                    allocator.free(item);
-                },
-                .least_response_time => |item| free_nested_slice(allocator, u8, item),
-                .random => |item| free_nested_slice(allocator, u8, item),
+                .round_robin => |rule| free_nested_slice(allocator, u8, rule),
+                .weighted_round_robin => |rule| free_ForwardWeighted_slice(allocator, rule),
+                .ip_hash => |rule| free_nested_slice(allocator, u8, rule),
+                .least_connections => |rule| free_nested_slice(allocator, u8, rule),
+                .weighted_least_connections => |rule| free_ForwardWeighted_slice(allocator, rule),
+                .least_response_time => |rule| free_nested_slice(allocator, u8, rule),
+                .random => |rule| free_nested_slice(allocator, u8, rule),
             }
         }
         allocator.free(self.bind);
+    }
+
+    fn vaildate_config(self: Config, allocator: std.mem.Allocator) !void {
+        var bind_address_map: std.StringHashMap(bool) = .init(allocator);
+        defer bind_address_map.deinit();
+        var target_address_map: std.StringHashMap(bool) = .init(allocator);
+        defer target_address_map.deinit();
+
+        for (self.bind) |item| {
+            if (bind_address_map.contains(item.address)) {
+                std.log.err("bind address: {s} duplicate", .{item.address});
+                return error.BindAddressDuplicate;
+            }
+            try bind_address_map.put(item.address, true);
+
+            switch (item.forward) {
+                .round_robin => |rule| try check_duplicate_nested_slice(&target_address_map, rule),
+                .weighted_round_robin => |rule| try check_duplicate_ForwardWeighted_slice(&target_address_map, rule),
+                .ip_hash => |rule| try check_duplicate_nested_slice(&target_address_map, rule),
+                .least_connections => |rule| try check_duplicate_nested_slice(&target_address_map, rule),
+                .weighted_least_connections => |rule| try check_duplicate_ForwardWeighted_slice(&target_address_map, rule),
+                .least_response_time => |rule| try check_duplicate_nested_slice(&target_address_map, rule),
+                .random => |rule| try check_duplicate_nested_slice(&target_address_map, rule),
+            }
+        }
+    }
+
+    fn free_nested_slice(allocator: std.mem.Allocator, T: type, s: []const []const T) void {
+        for (s) |item| {
+            allocator.free(item);
+        }
+        allocator.free(s);
+    }
+
+    fn free_ForwardWeighted_slice(allocator: std.mem.Allocator, s: []const ForwardWeighted) void {
+        for (s) |item| {
+            allocator.free(item.target);
+        }
+        allocator.free(s);
+    }
+
+    fn check_duplicate_nested_slice(map: *std.StringHashMap(bool), s: []const []const u8) !void {
+        map.clearAndFree();
+        for (s) |target| {
+            if (map.contains(target)) {
+                std.log.err("target address: {s} duplicate", .{target});
+                return error.TargetAddressDuplicate;
+            }
+            try map.put(target, true);
+        }
+    }
+
+    fn check_duplicate_ForwardWeighted_slice(map: *std.StringHashMap(bool), s: []const ForwardWeighted) !void {
+        map.clearAndFree();
+        for (s) |item| {
+            if (map.contains(item.target)) {
+                std.log.err("target address: {s} duplicate", .{item.target});
+                return error.TargetAddressDuplicate;
+            }
+            try map.put(item.target, true);
+        }
     }
 };
 
@@ -110,12 +161,7 @@ pub fn get_config(allocator: std.mem.Allocator) !Config {
         return error.ConfigParseError;
     };
 
-    return config;
-}
+    try config.vaildate_config(allocator);
 
-fn free_nested_slice(allocator: std.mem.Allocator, T: type, s: []const []const T) void {
-    for (s) |item| {
-        allocator.free(item);
-    }
-    allocator.free(s);
+    return config;
 }
